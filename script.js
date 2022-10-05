748a9930f3cf4e6d95c73e7b350b1281527f7c34
Rui Silva <ruisinhofilipe@gmail.com>
2022-10-05 12:15:53 +0200

    Operators working as expected

--- script.js
+++ script.js
@@ -1,76 +1,79 @@
-function add(x, y) {
-    return x + y;
-}
-
-function subtract(x, y) {
-    return x + y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    return x / y;
-}
-
-let display = document.querySelector('.currentOutput');
-let previousDisplay = document.querySelector('.previousOutput')
-let equal = document.querySelector('.equal');
-let deleteAll = document.querySelector('.delete');
-let clear = document.querySelector('.clear');
-let numbers = document.querySelectorAll('.numbers');
-let operators = document.querySelectorAll('.operators');
-let array = [];
-let mainValue = 0;
 let currentValue = '';
 let previousValue = '';
-let mainOperator = '';
+let op = '';
 
+let numbers = document.querySelectorAll('.numbers');
+let operators = document.querySelectorAll('.operators');
+let displayCurrentValue = document.querySelector('.displayCurrentValue');
+let displayPreviousValue = document.querySelector('.displayPreviousValue');
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         currentValue += number.id;
-        display.textContent = currentValue;
-    })
+        displayCurrentValue.textContent = currentValue;
+    });
 });
 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        mainOperator = operator.id;
-        previousValue = currentValue;
-        currentValue = '';
-        array.push(previousValue);
-        array.push(mainOperator);
-        previousDisplay.textContent = array.join(' ');
-    })
+        if (currentValue === '') {
+            op = operator.id;
+            displayPreviousValue.textContent = `${previousValue} ${op}`;
+            displayCurrentValue.textContent = '';
+            currentValue = '';
+            console.log(currentValue);
+            console.log(previousValue);
+        } else if (previousValue === '') {
+            op = operator.id;
+            previousValue = currentValue;
+            displayPreviousValue.textContent = `${previousValue} ${op}`;
+            displayCurrentValue.textContent = '';
+            currentValue = '';
+        } else {
+            calculate();
+            op = operator.id;
+            displayPreviousValue.textContent = `${previousValue} ${op}`;
+            displayCurrentValue.textContent = '';
+        }
+    });
 });
 
-clear.addEventListener('click', () => {
-    currentValue = currentValue.slice(0, currentValue.length - 1);
-    display.textContent = currentValue;
+let erase = document.querySelector('.delete');
+erase.addEventListener('click', () => {
+    currentValue = '';
+    previousValue = '';
+    op = '';
+    displayCurrentValue.textContent = '';
+    displayPreviousValue.textContent = '';
 })
 
+let equal = document.querySelector('.equal');
 equal.addEventListener('click', () => {
-    calculate();
-})
+    displayPreviousValue.textContent = '';
+});
 
 function calculate() {
     previousValue = Number(previousValue);
     currentValue = Number(currentValue);
-    if (mainOperator === '+') {
-        mainValue += previousValue + currentValue;
-    } else if (mainOperator === '-') {
-        mainValue += previousValue - currentValue;
-    } else if (mainOperator === '/') {
-        mainValue /= previousValue / currentValue;
-    } else if (mainOperator === '*') {
-        mainValue *= previousValue * currentValue;
+    if (op === '+') {
+        previousValue += currentValue;
+    } else if (op === '-') {
+        previousValue -= currentValue;
+    } else if (op === '*') {
+        previousValue *= currentValue;
+    } else if (op === '/') {
+        previousValue /= currentValue;
     }
-    array.push(currentValue);
-    previousDisplay.textContent = array.join(' ');
-    display.textContent = mainValue;
-    previousValue = '';
     currentValue = '';
-    mainOperator = '';
+    op = '';
+    previousValue = previousValue.toString();
+    displayCurrentValue.textContent = previousValue;
 }
+
+
+
+
+
+
+
+
